25a1133c87f76daa2ab444f27f262a1f674f6997
qcuong03 <cuongbao2003@gmail.com>
2023-09-13 16:14:54 +0700

    first commit

---
+++ THWeb_Bai1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace THWeb_Bai1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ THWeb_Bai1/Models/Gender.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace THWeb_Bai1.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    } 
+}
